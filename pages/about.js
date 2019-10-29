27fe79871a094225c96fc7aafc1b44554175a46c
Dylan Stein <djstein@ncsu.edu>
2019-10-28 19:36:46 -0700

    base next.js page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../components/BaseLayout";
+
+const About = () => (
+  <Layout>
+    <h1>About</h1>
+  </Layout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../components/BaseLayout";
+
+const Index = () => (
+  <Layout>
+    <h1>Settings Up</h1>
+  </Layout>
+);
+
+export default Index;
